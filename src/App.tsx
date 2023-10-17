2f49c9daa9a6e63549ac92004b3cfaba25dfa53e
Maxim_ <maximprosin@gmail.com>
2023-10-18 00:35:53 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import HomePage from './pages/HomePage/HomePage'
 import TodoList from './pages/TodoList/TodoList'
 
+import { Route, Routes } from "react-router-dom";
+
 import './App.css'
 
 function App() {
   return (
     <>
-      {/* <HomePage /> */}
-      <TodoList/>
+      <Routes>
+        <Route path="*" element={<TodoList />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
